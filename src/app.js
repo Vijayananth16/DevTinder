6044b6f74b30ef8efc7b755bcd8382509efc959a
Vijayananth16 <142078524+Vijayananth16@users.noreply.github.com>
2025-03-19 08:33:59 +0530

    routing practising

--- src/app.js
+++ src/app.js
@@ -2,12 +2,36 @@ const express=require("express")
 
 const app=express()
 
-app.use("/test",(req,res)=>{
-    res.send("'test' from the server nodemon")
+app.use("/user",(req,res)=>{
+    res.send("eeeeeeeeeehhhhheeeeee")
 })
-app.use("/hello",(req,res)=>{
-    res.send("'hello' from the server ")
+app.get("/user",(req,res)=>{
+    res.send({"firstName":"vijay","secondName":"Anand"})
 })
+
+app.post("/user",(req,res)=>{
+    res.send("insert successfully")
+})
+app.put("/user",(req,res)=>{
+    res.send("updated successfully")
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("deleted")
+})
+// app.use("/",(req,res)=>{
+//     res.send("'default' from the server nodemon")
+// })
+// app.use("/test",(req,res)=>{
+//     res.send("'test' from the server nodemon")
+// })
+
+// // app.use("/hello/2",(req,res)=>{
+// //     res.send("'hello 2'  ")
+// // })
+// app.use("/hello",(req,res)=>{
+//     res.send("'hello' from the server ")
+// })
 app.listen(1818,()=>{
     console.log("Server is Successfully learning in port 1818")
 })
